9a06494be0e42905375778dc55e4724eedaed813
habib <habib@thebosstech.com>
2020-12-21 11:43:04 -0800

    final change accroding to current env

--- app/Data.php
+++ app/Data.php
@@ -2,6 +2,6 @@
 namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Data extends Model {
-	protected $table = 'bootgrid_data';
+	protected $table = 'datatables_data';
 	public $timestamps = false;
 }
